ca023d29d3e7bbd72b05c1a0ed633421d456bf07
Aman Verma <amanverma805227@gmail.com>
2026-09-08 07:17:40 +0530

    Update 0258-add-digits.cpp

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,17 +1,10 @@
-class Solution {
-public:
-    int addDigits(int num) {
-
-        while(num>9){
-
-        int rem, ans = 0;
-        while(num!=0){
-            rem = num%10;
-            num/= 10;
-            ans += rem;
-        }
-        num = ans;
-    }
-    return num;
-    }
-};
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            ans = 0
+            while num != 0:
+                rem = num % 10
+                num //= 10  # Use integer division in Python
+                ans += rem
+            num = ans
+        return num
